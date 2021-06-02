703206711dc9ec9841d33ea2d39a9a1ba5e10a4b
RyanApolinario <51379858+RyanApolinario@users.noreply.github.com>
2021-06-02 11:27:52 -0300

    Add files via upload

---
+++ Atv3.sql
@@ -0,0 +1,72 @@
+--  01 
+DELIMITER $$
+
+CREATE FUNCTION Inserir_Aluno(codCurso INT, dataNasc DATE, totalCred INT, mgp DOUBLE(10,2),
+                                nomeAluno VARCHAR(60), emailAluno VARCHAR(50)) RETURNS VARCHAR(60)
+
+DETERMINISTIC
+
+BEGIN
+    INSERT INTO aluno (cod_curso, dat_nasc, tot_cred, mgp, nom_alun, email) VALUES (codCurso, dataNasc, totalCred, mgp, nomeAluno, emailAluno);
+
+    RETURN nomeAluno;
+
+END
+
+$$
+DELIMITER ;
+
+
+
+--  02
+DELIMITER $$
+
+CREATE PROCEDURE SP_While()
+
+BEGIN
+
+    DECLARE Contador int default 1;
+    DECLARE Saida VARCHAR(20) DEFAULT "";
+
+    WHILE Contador <= 5 DO
+    
+        SET Saida = CONCAT(Saida, Contador, ", ");
+        SET Contador = Contador +  1;
+        
+    END WHILE; 
+   
+   SELECT Saida;
+   
+END
+$$
+DELIMITER ;
+
+CALL SP_While();
+
+
+
+
+--  03
+DELIMITER $$
+
+CREATE PROCEDURE sp_Repeat()
+
+BEGIN
+
+    DECLARE contador INT DEFAULT 1;
+    DECLARE Resultado VARCHAR(50) DEFAULT "";
+
+    REPEAT
+        SET Resultado = CONCAT(Resultado, contador, ",");
+        SET contador = contador + 1;
+        until contador > 9
+    END REPEAT;
+
+    SELECT Resultado;
+
+END
+
+$$
+DELIMITER ;
+
+CALL sp_repeat();
